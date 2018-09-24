70a5079e016208e82ecf2f7b3a722f4370a81ce4
Naga Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-09-24 11:44:41 +0000

    test823 New files added - [CountryInfoService,MyJavaService,classicmodels,googleapis,hrdb,wavemakeronline,googlemaps,youtube,i18n,i18n] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,securityService,WEB-INF,data,lib,logos,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,dependencies.json,pom.xml,README.md,Readme.txt,.wmproject.properties,README.md]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "Test_New",
+  "homePage" : "Main",
+  "name" : "Test_New",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "de,en,kn,mr",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
